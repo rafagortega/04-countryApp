84c9d5072ada073b26faefca23fc86dcaa230e0e
rafaGOrtega <rafagao95@gmail.com>
2023-08-22 15:31:40 +0200

    informacion de pais desplegada
    En el servicio se termina el metodo para consultar información de cada país por id. Esa información se despliega en su página y además se habilita el enlace "Ver mas" para que en la tabla donde se muestran los paises, este enlace lleve a la pagina de país correspondiente.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
   private APIUrl:string='https://restcountries.com/v3.1'
   constructor(private http:HttpClient) { }
+
+  searchCountryByAlphaCode(code:string):Observable<Country|null>{
+    const url=`${this.APIUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url)
+    .pipe(
+      map(countries=>countries.length>0 ? countries[0]:null),
+      catchError(error=>of(null))
+    );
+  }
 
   searchCapital(term:string):Observable<Country[]>{
     const url=`${this.APIUrl}/capital/${term}`;
